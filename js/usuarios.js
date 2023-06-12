36fe9380d03b94916a9241bf6befecb8bfda6cf2
solarroyoportfolio <125100413+solarroyoportfolio@users.noreply.github.com>
2023-06-11 23:46:20 -0300

    BR v1.2

--- js/usuarios.js
+++ js/usuarios.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     let nombre = localStorage.getItem("username");
     if (nombre == "sol") {
         let encabezado = document.getElementById("Encabezado");
-        encabezado.innerHTML = "<img src='/img/sol.png' alt='Imagen-sol' style='width:70px; border-radius:50%''> Hola " + nombre + " 👋🏽";
+        encabezado.innerHTML = "<img src='../img/sol.png' alt='Imagen-sol' style='width:70px; border-radius:50%''> Hola " + nombre + " 👋🏽";
       }
 
     else{
       let encabezado = document.getElementById("Encabezado");
-      encabezado.innerHTML = "<img src=' /img/"+nombre+".png' alt='Imagen-usuario' style='width:70px; border-radius:50%''> Hola " + nombre + " 👋🏽";
+      encabezado.innerHTML = "<img src=' ../img/"+nombre+".png' alt='Imagen-usuario' style='width:70px; border-radius:50%''> Hola " + nombre + " 👋🏽";
     }
   });
