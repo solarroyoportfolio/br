50708bc7951048e1f3485a241635ea6aeb4e12ad
solarroyoportfolio <125100413+solarroyoportfolio@users.noreply.github.com>
2023-06-12 21:12:59 -0300

    br v1.4

--- js/usuarios.js
+++ js/usuarios.js
@@ -10,3 +10,49 @@ document.addEventListener('DOMContentLoaded', function() {
       encabezado.innerHTML = "<img src='../img/"+nombre+".png' alt='Imagen-usuario' style='width:70px; border-radius:50%''> Hola " + nombre + " 👋🏽";
     }
   });
+
+
+  // document.addEventListener('DOMContentLoaded', function() {
+   
+  //   let usuario = localStorage.getItem("username");
+
+  //   if ( (usuario!="sol") && (usuario!="maia"))
+  //   {
+  //     if (usuario=="sofi") {
+  //       let vista_sofi = document.getElementById("tabla-sofi");
+  //       let estilosofi = window.getComputedStyle(vista_sofi);
+  //       let vista_magui = document.getElementsByClassName("tabla-magui");
+  //       let estilomagui = window.getComputedStyle(vista_magui);
+
+  //       let vista_clau = document.getElementsByClassName("tabla-clau");
+  //       let estiloclau = window.getComputedStyle(vista_clau);
+
+  //       let vista_agus = document.getElementsByClassName("tabla-agus");
+  //       let estiloagus = window.getComputedStyle(vista_agus);
+
+  //       if(estilosofi.display==="none"){
+  //         vista_sofi.style.display ="block" ;
+  //       }
+
+  //         vista_magui.style.display ="none" ;
+
+        
+
+      
+  //         vista_clau.style.display ="none" ;
+
+      
+
+      
+  //         vista_agus.style.display ="none " ;
+
+        
+
+
+
+
+
+  //     }
+  //   }
+    
+  // });
